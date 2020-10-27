a080e281654229cb82d319eb71ca192f898e3ae0
Jsgtaj <jasaj316@gmail.com>
2020-10-27 17:11:40 -0400

    fixing json

--- functions/poslog.js
+++ functions/poslog.js
@@ -1,8 +1,8 @@
-const latLong = [];
+let latLong = [0, 0];
 exports.handler = async event => {
-  const lat = event.queryStringParameters.lat || 0;
+  const lat = event.queryStringParameters?.lat || 0;
   const long = event.queryStringParameters?.long || 0;
-  latLong.push(lat, long);
+  latLong = [lat, long];
 
   const json = JSON.stringify(latLong);
   return {
