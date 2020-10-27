ff83e7f269446de78aab8fc74bd23cbe8e56b955
Jsgtaj <jasaj316@gmail.com>
2020-10-27 18:48:56 -0400

    added mobile if statement

--- functions/poslog.js
+++ functions/poslog.js
@@ -1,6 +1,7 @@
 let latLong = [0, 0];
 exports.handler = async event => {
   const { lat, long, mobile } = event.queryStringParameters;
+  // only update map if using mobile
   if (mobile) {
     latLong = [lat, long];
   }
