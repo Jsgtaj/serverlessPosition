dbbd22d771647970a42372cdd73a7e02304419f6
Jsgtaj <jasaj316@gmail.com>
2020-10-27 18:31:11 -0400

    added mobile param to get

--- functions/poslog.js
+++ functions/poslog.js
@@ -1,8 +1,9 @@
 let latLong = [0, 0];
 exports.handler = async event => {
-  const { lat, long } = event.queryStringParameters;
-  latLong = [lat, long];
-
+  const { lat, long, mobile } = event.queryStringParameters;
+  if (mobile) {
+    latLong = [lat, long];
+  }
   const json = JSON.stringify(latLong);
   return {
     statusCode: 200,
